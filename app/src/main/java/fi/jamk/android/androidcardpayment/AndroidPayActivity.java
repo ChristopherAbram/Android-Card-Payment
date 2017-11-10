acc3ed0bb604cf8564d4d6f54604e640d3a0b49c
Christopher Abram <krzysztofabram1@gmail.com>
2017-11-10 19:15:59 +0200

    CardFormActivity initiated, created AndroidPayActivity

---
+++ app/src/main/java/fi/jamk/android/androidcardpayment/AndroidPayActivity.java
@@ -0,0 +1,13 @@
+package fi.jamk.android.androidcardpayment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidPayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_pay);
+    }
+}
--- app/src/main/java/fi/jamk/android/androidcardpayment/CardFormActivity.java
+++ app/src/main/java/fi/jamk/android/androidcardpayment/CardFormActivity.java
@@ -1,13 +1,99 @@
 package fi.jamk.android.androidcardpayment;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
-public class CardFormActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wallet.FullWallet;
+import com.google.android.gms.wallet.MaskedWallet;
+import com.google.android.gms.wallet.WalletConstants;
+import com.simplify.android.sdk.CardEditor;
+import com.simplify.android.sdk.Simplify;
+
+public class CardFormActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, Simplify.AndroidPayCallback {
+
+    static final String TAG = CardFormActivity.class.getSimpleName();
+    static final String WALLET_FRAGMENT_ID = "wallet_fragment";
+
+    private GoogleApiClient mGoogleApiClient;
+    private CardEditor mCardEditor;
+    private Button mPayButton;
+    private Simplify simplify;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_form);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mGoogleApiClient != null && !mGoogleApiClient.isConnected())
+            mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        if(mGoogleApiClient != null && mGoogleApiClient.isConnected())
+            mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // let the Simplify SDK marshall out the android pay activity results
+        if (simplify.handleAndroidPayResult(requestCode, resultCode, data, this))
+            return;
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onReceivedMaskedWallet(MaskedWallet maskedWallet) {
+        // launch confirmation activity
+        Intent intent = new Intent(getApplicationContext(), AndroidPayActivity.class);
+        intent.putExtra(WalletConstants.EXTRA_MASKED_WALLET, maskedWallet);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onReceivedFullWallet(FullWallet fullWallet) {
+
+    }
+
+    @Override
+    public void onAndroidPayCancelled() {
+
+    }
+
+    @Override
+    public void onAndroidPayError(int i) {
+
     }
 }
